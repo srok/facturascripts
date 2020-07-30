dcb6c0230825d29c0de0d4fc6f6cdd0415100762
francot <francotuni@srok.com.ar>
2020-07-30 09:57:43 -0300

    Revert "quitar Dinamic"
    This reverts commit d41ba778fef9b70c8b3272a241b6118afd40f690.

---
+++ Dinamic/Model/ModelView/LiquidacionComisionFactura.php
@@ -0,0 +1,9 @@
+<?php namespace FacturaScripts\Dinamic\Model\ModelView;
+
+/**
+ * Class created by Core/Base/PluginManager
+ * @author FacturaScripts <carlos@facturascripts.com>
+ */
+class LiquidacionComisionFactura extends \FacturaScripts\Core\Model\ModelView\LiquidacionComisionFactura
+{
+}
